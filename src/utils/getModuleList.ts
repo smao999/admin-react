8abd175b00fcbce10cd015eb73a82e0d99d7f80a
xiaqingsong <927538064@qq.com>
2023-11-28 03:59:09 +0800

    注释

--- src/utils/getModuleList.ts
+++ src/utils/getModuleList.ts
@@ -1,6 +1,6 @@
-// 根据routers下的route文件动态生成路由
 import { AppRouteObject } from '@/types/router';
 
+// 根据routers下的route文件动态生成所有路由
 export const getModuleList = () => {
 	const res: AppRouteObject[] = [];
 	const modules = import.meta.glob('../routers/route/**/*.tsx', {
@@ -15,6 +15,7 @@ export const getModuleList = () => {
 	return res;
 };
 
+// 数据库返回，暂时硬编码
 const defaultAuthList = ['/'];
 
 // 根据权限动态生成路由
@@ -26,6 +27,7 @@ export const filterMenu = (
 	if (authList.length > 0 && authList[0] === '/') {
 		return allModuleList;
 	}
+	// 父元素的key无权限则整个子元素都无权限不进行判断
 	return allModuleList
 		.filter(
 			item => authList.includes(item.meta?.key as string) || item.index,
